59dfb6383801235aeaf4541aab8a65f1b6930794
Lusorich <dreamonautre@gmail.com>
2019-12-03 00:11:47 +0500

    change

--- just-fun/app/app.js
+++ just-fun/app/app.js
@@ -1,7 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    res.write("<body><div>Page</div><form method='POST' action='/message'><input name='msg' type='text'><button type='submit'>Send</button></input></form></body>");
+    return res.end();
+  } 
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY');
+    console.log(req);
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
 });
 
 server.listen(3012);
