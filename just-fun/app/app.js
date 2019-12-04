24c1c7bdc239a8fa0e91376a3a23800a3fb2c0b1
Lusorich <dreamonautre@gmail.com>
2019-12-05 00:14:01 +0500

    change

--- just-fun/app/app.js
+++ just-fun/app/app.js
@@ -2,18 +2,24 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
+  const { url, method } = req;
   if (url === '/') {
     res.write("<body><div>Page</div><form method='POST' action='/message'><input name='msg' type='text'><button type='submit'>Send</button></input></form></body>");
     return res.end();
   } 
-  if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
-    console.log(req);
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+  if (url === '/message' && method === 'POST') { 
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+    });
   }
 });
 
