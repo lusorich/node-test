4e3f8b49435d2ac9a4dba02ead1681a42b23d41f
Lusorich <dreamonautre@gmail.com>
2020-01-26 22:10:34 +0500

    fun

--- just-fun/express/app.js
+++ just-fun/express/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyparser = require("body-parser");
 const path = require("path");
-const expressHbs = require("express-handlebars");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -11,15 +10,14 @@ const rootDir = require("./util/path");
 
 const app = express();
 
-app.engine("hbs", expressHbs({ defaultLayout: "main-layout", extname: "hbs" }));
-app.set("view engine", "hbs");
-app.set("views", "views");
+app.set("view engine", "pug");
+app.set("vie  ws", "views");
 
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/admin", adminData.router);
-app.use(shopRoutes);
+app.use("/shop", shopRoutes);
 app.use(page404);
 
 app.listen(3030);
