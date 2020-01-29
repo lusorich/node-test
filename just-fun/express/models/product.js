faa244d476faefba6261cacc037817dd4c95d0f2
Lusorich <dreamonautre@gmail.com>
2020-01-29 22:23:21 +0500

    fun

--- just-fun/express/models/product.js
+++ just-fun/express/models/product.js
@@ -1,22 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
+const pathDir = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(pathDir, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const pathDir = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(pathDir, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(pathDir, JSON.stringify(products), err => {
         console.log(err);
@@ -25,16 +31,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const pathDir = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(pathDir, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
