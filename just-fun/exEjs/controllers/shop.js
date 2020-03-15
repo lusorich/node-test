92a02c87c0d6fdfb1940b8ec036004591caee4b2
Lusorich <dreamonautre@gmail.com>
2020-03-15 21:57:47 +0500

    fun

--- just-fun/exEjs/controllers/shop.js
+++ just-fun/exEjs/controllers/shop.js
@@ -15,14 +15,15 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
-      product: product,
-      pageTitle: product.title,
-      path: '/products'
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All products',
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
